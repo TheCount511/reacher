4314e804322fad9d5249f5aa0569d8e0152b8cd3
TheCount511 <fadhil.akins@gmail.com>
2022-10-22 21:46:44 -0600

    few changes made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AddIcon from "@mui/icons-material/Add";
+import { styled, Typography } from "@mui/material";
 
 function App() {
+  const BlueButton = styled(Button)({
+    backgroundColor: "skyblue",
+    color: "#888",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue",
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white",
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="text">Text</Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        size="small"
+        startIcon={<SettingsIcon />}
+      >
+        Contained
+      </Button>
+      <Button
+        variant="contained"
+        color="success"
+        size="small"
+        startIcon={<AddIcon />}
+      >
+        Contained
+      </Button>
+      <Button variant="outlined">Outlined</Button>
+      <Typography variant="h1" component="h2">
+        h1.Heading
+      </Typography>
+      <BlueButton variant="contained" disabled>
+        unique button
+      </BlueButton>
+      <BlueButton variant="contained">
+        My button
+      </BlueButton>
     </div>
   );
 }
