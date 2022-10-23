b1e0cf71971c56c7e2205d57d5fbab6e3c54d6e5
TheCount511 <fadhil.akins@gmail.com>
2022-10-23 15:58:53 -0600

    development addet to ui

--- src/components/feed/Feeds.jsx
+++ src/components/feed/Feeds.jsx
@@ -1,9 +1,58 @@
-import { Box } from "@mui/material"
+import { Favorite, MoreVert, Share } from "@mui/icons-material";
+import {
+  Box,
+  Card,
+  CardHeader,
+  Avatar,
+  IconButton,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+} from "@mui/material";
 
-const Feed =()=>{
-    return(
-        <Box bgcolor={"red"} flex={4} p={2} >Feed</Box>
-    )
-}
+const Feed = () => {
+  return (
+    <Box flex={4} p={2}>
+      <Card>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVert />
+            </IconButton>
+          }
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          component="img"
+          height="20%"
+          image="https://www.thestar.com/content/dam/thestar/edmonton/2018/07/19/backlash-over-edmonton-food-bloggers-cheese-plate-review-leaves-a-bad-taste-for-everyone/cheese_board.jpg"
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <Favorite />
+          </IconButton>
+          <IconButton aria-label="share">
+            <Share />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+};
 
-export default Feed
+export default Feed;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,10 +6,13 @@ import {
   InputBase,
   Badge,
   Avatar,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import { styled } from "@mui/system";
 import StorefrontIcon from "@mui/icons-material/Storefront";
 import { Mail, Notifications } from "@mui/icons-material";
+import { useState } from "react";
 
 const Navbar = () => {
   const StyledToolbar = styled(Toolbar)({
@@ -25,16 +28,25 @@ const Navbar = () => {
   }));
 
   const Icons = styled(Box)(({ theme }) => ({
-    display: "flex",
+    display: "none",
     gap: "20px",
     alignItems: "center",
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
+    },
   }));
 
   const UserBox = styled(Box)(({ theme }) => ({
     display: "flex",
     gap: "10px",
     alignItems: "center",
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+    cursor:"pointer"
   }));
+
+  const [open, setOpen] = useState(false);
   return (
     <AppBar position="sticky">
       <StyledToolbar>
@@ -43,7 +55,7 @@ const Navbar = () => {
         </Typography>
         <StorefrontIcon sx={{ display: { xs: "block", sm: "none" } }} />
         <Search>
-          <InputBase placeholder="Search..." />{" "}
+          <InputBase placeholder="Search..." />
         </Search>
         <Icons>
           <Badge badgeContent={4} color="error">
@@ -53,18 +65,38 @@ const Navbar = () => {
             <Notifications />
           </Badge>
           <Avatar
+            onClick={() => setOpen(true)}
             src="https://www.w3schools.com/w3images/avatar6.png"
-            sx={{ width: 30, height: 30 }}
+            sx={{ width: 30, height: 30, cursor:"pointer" }}
           />
         </Icons>
-        <UserBox>
+        <UserBox onClick={() => setOpen(true)}>
           <Avatar
             src="https://www.w3schools.com/w3images/avatar6.png"
-            sx={{ width: 30, height: 30 }}
+            sx={{ width: 30, height: 30,cursor:"pointer" }}
+            onClick={() => setOpen(true)}
           />
           <Typography variant="span">John</Typography>
         </UserBox>
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={() => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
